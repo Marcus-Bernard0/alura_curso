c59eb3aab768a74f59f09fdbce29988d11a19bc9
Marcus Bernardo <84023514+Marcus-Bernard0@users.noreply.github.com>
2022-06-13 21:53:50 -0300

    inciando curso oo 2

---
+++ oo_2/modelo.py
@@ -0,0 +1,21 @@
+from mailbox import NotEmptyError
+
+
+class Filme():
+    def __init__(self, nome, ano, duracao):
+        self.nome = nome
+        self.ano = ano
+        self.duracao = duracao
+
+filme1 = Filme("batman", 2022, 300)
+print(f'O nome é {filme1.nome}.')
+    
+
+class Serie():
+    def __init__(self, nome, ano,temporada):
+        self.nome = nome
+        self.ano = ano
+        self.temporada = temporada
+
+serie1 = Serie("Peak Blinders" , 2022, 6)
+print(f'O nome é {serie1.nome}, lançada {serie1.temporada} temporadas, com a última em {serie1.ano}')
