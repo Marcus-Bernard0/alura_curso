f2aeb5e192483ca27cd8bcf97a24067cd039c3f2
Marcus Bernardo <84023514+Marcus-Bernard0@users.noreply.github.com>
2022-06-13 22:17:11 -0300

    criando objeto de likes

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -3,19 +3,34 @@ from mailbox import NotEmptyError
 
 class Filme():
     def __init__(self, nome, ano, duracao):
-        self.nome = nome
+        self.nome = nome.title()
         self.ano = ano
         self.duracao = duracao
+        self.likes = 0
 
-filme1 = Filme("batman", 2022, 300)
-print(f'O nome é {filme1.nome} lançado em {filme1.ano}')
-    
+    # adicionado novo método para contagem e novos likes
+    def dar_likes(self):
+        self.likes += 1
+
 
 class Serie():
-    def __init__(self, nome, ano,temporada):
+    def __init__(self, nome, ano, temporada):
         self.nome = nome
         self.ano = ano
         self.temporada = temporada
+        self.likes = 0
 
-serie1 = Serie("Peak Blinders" , 2022, 6)
-print(f'O nome é {serie1.nome}, lançada {serie1.temporada} temporadas, com a última em {serie1.ano}')
+    def dar_likes(self):
+        self.likes += 1
+
+
+filme1 = Filme("batman", 2022, 300)
+
+# dando like no filme
+
+print(
+    f'O nome é {filme1.nome} lançado em {filme1.ano}. {filme1.likes} pessoas gostaram')
+
+serie1 = Serie("Peak Blinders", 2022, 6)
+print(
+    f'O nome é {serie1.nome}, lançada {serie1.temporada} temporadas, com a última em {serie1.ano}')
