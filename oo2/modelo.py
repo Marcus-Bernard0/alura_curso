1467a52ce2e44b2e2c36533607609c9956cfa283
Marcus Bernardo <84023514+Marcus-Bernard0@users.noreply.github.com>
2022-06-15 21:41:08 -0300

    inserindo herança no código

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -1,60 +1,57 @@
 from mailbox import NotEmptyError
 from xml.sax.handler import property_declaration_handler
 
+#criando herança
+class Programa:
+    def __init__(self, nome, ano ):
+        self._nome = nome
+        self._ano = ano
+        self._likes = 0
 
-class Filme():
+#adicionado novo método para contagem e novos likes
+    @property
+    def likes(self):
+        return self._likes 
+
+    def dar_likes(self):
+        self._likes += 1
+
+    @property
+    def nome(self):
+        return self._nome.title()
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome.title()
+
+
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.duracao = duracao
-        self.__likes = 0
+        self._likes = 0
 
-    # adicionado novo método para contagem e novos likes
-    @property
-    def likes(self):
-        return self.__likes 
-
-    def dar_likes(self):
-        self.__likes += 1
-
-    @property
-    def nome(self):
-        return self.__nome.title()
-
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome.title()
+    
 
 
-class Serie():
+class Serie(Programa):
     def __init__(self, nome, ano, temporada):
-        self.__nome = nome.title()
+        self._nome = nome.title()
         self.ano = ano
         self.temporada = temporada
-        self.__likes = 0
+        self._likes = 0
 
-    def dar_likes(self):
-        self.likes += 1
+   
 
-    @property 
-    def likes(self):
-        return self.__likes
-
-    @property
-    def nome(self):
-        return self.__nome
-    
-    @nome.setter
-    def nome(self, novo_nome):
-       self.__nome = novo_nome.title()
-
-
+#atribuindo dados
 filme1 = Filme("batman das trevas", 2022, 300)
 
 # dando like no filme
 filme1.dar_likes()
 print(
     f'O nome é {filme1.nome} lançado em {filme1.ano}. {filme1.likes} pessoas gostaram')
+
 
 serie1 = Serie("peak blinders", 2022, 6)
 #usando set para mudar nome
