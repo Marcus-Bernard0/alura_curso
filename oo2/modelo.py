c8faac9b3d81a1784c7318d5ce979ab40565c623
Marcus Bernardo <84023514+Marcus-Bernard0@users.noreply.github.com>
2022-06-18 19:28:58 -0300

    usando herança built in list no objeto playlist

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -1,16 +1,16 @@
 
-#criando herança
-#classe mãe
+# criando herança
+# classe mãe
 class Programa:
-    def __init__(self, nome, ano ):
+    def __init__(self, nome, ano):
         self._nome = nome.title()
         self._ano = ano
         self._likes = 0
 
-#adicionado novo método para contagem e novos likes
+# adicionado novo método para contagem e novos likes
     @property
     def likes(self):
-        return self._likes 
+        return self._likes
 
     def dar_likes(self):
         self._likes += 1
@@ -23,8 +23,8 @@ class Programa:
     def nome(self, novo_nome):
         self._nome = novo_nome.title()
 
-    #criando método para polimorfismo
-     #def imprime(self):
+    # criando método para polimorfismo
+     # def imprime(self):
         #print(f'{self._nome} - {self._ano} - {self.duracao} - {self._likes}')
 
     def __str__(self):
@@ -33,18 +33,18 @@ class Programa:
 
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        #inserindo superclasse para remover erros
+        # inserindo superclasse para remover erros
         super().__init__(nome, ano)
         #self._nome = nome.title()
         #self.ano = ano
         self.duracao = duracao
         #self._likes = 0
-        
-   
-    #defindo o método com __str__
-    
+
+    # defindo o método com __str__
+
     def __str__(self):
         return f'{self._nome} - lançado em {self._ano} - {self.duracao} min - {self._likes} likes'
+
 
 class Serie(Programa):
     def __init__(self, nome, ano, temporada):
@@ -55,28 +55,24 @@ class Serie(Programa):
         #self._likes = 0
 
     def __str__(self):
-       return f'{self._nome} - lançado em {self._ano} - {self.temporada} min - {self._likes} likes'
+        return f'{self._nome} - lançado em {self._ano} - {self.temporada} min - {self._likes} likes'
 
 
-
-class Playlist:
+class Playlist(list):
     def __init__(self, nome, programas):
         self.nome = nome
         self.programas = programas
-        
-    #retorna quantidade de programas
-    def tamanho (self, tamanho):
-        self.tamanho = len(tamanho)
+        super().__init__(programas)
+        #herdando a list (bult in), objeto passa ser interável
 
-       
 
-#atribuindo dados
+# atribuindo dados
 filme1 = Filme("homem aranha - de volta para a casa", 2022, 300)
 filme2 = Filme("velozes e furiosos", 2009, 160)
 filme3 = Filme("procurando nemo", 2009, 90)
 
 print({filme2.nome})
-#dando like 
+# dando like
 filme1.dar_likes()
 filme2.dar_likes()
 filme2.dar_likes()
@@ -86,7 +82,7 @@ filme2.dar_likes()
 
 serie1 = Serie("peak blinders", 2022, 6)
 
-#usando set para mudar nome
+# usando set para mudar nome
 serie1.nome = "Eu a patroa e as crianças"
 serie1.dar_likes()
 serie1.dar_likes()
@@ -94,27 +90,31 @@ serie1.dar_likes()
 
 print(26*'###')
 
-#criando uma lista e armazenando variáveis filme e serie
+# criando uma lista e armazenando variáveis filme e serie
 filmes_e_series = [filme1, serie1, filme2, filme3]
 
 playlist_fim_de_semana = Playlist('Fim de semana', filmes_e_series)
 
-#polimorfismo, listas do mesmo tipo. Ou seja, filme e serie contém compartilham variáveis como
-#nome e likes
+# polimorfismo, listas do mesmo tipo. Ou seja, filme e serie contém compartilham variáveis como
+# nome e likes
 mensagem = "Filmes contam com duração em minutos e série temporadas."
 mensagem = mensagem.upper()
 print(f'{mensagem} \n')
 
-for programa in playlist_fim_de_semana.programas:
+print(f"Tamanho da playlist: {len(playlist_fim_de_semana)}")
+for programa in playlist_fim_de_semana:
     print(programa)
 
-    #programa.imprime()
-    #criando variável para imprimir especificade do filme ou serie (temporada, duracao)
-    #foi criado um método na classe mãe para facilitar o polimorfismo, ou seja
-    #quanto mais especificidade, mas if's seriam necessários
+    # programa.imprime()
+    # criando variável para imprimir especificade do filme ou serie (temporada, duracao)
+    # foi criado um método na classe mãe para facilitar o polimorfismo, ou seja
+    # quanto mais especificidade, mas if's seriam necessários
 
     '''
     detalhes = programa.duracao if hasattr(programa, 'duracao') else programa.temporada
     print(f'{programa._nome} - D {detalhes} - {programa.likes}')'''
 
-    
+
+#verificando se tem certo filme ou série na playlist
+
+
