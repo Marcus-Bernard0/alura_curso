7dae54c29ddd835a97b32bb05557939a7ea8ef73
Marcus Bernardo <84023514+Marcus-Bernard0@users.noreply.github.com>
2022-06-19 19:46:12 -0300

    exercicio abc

--- oo2/abc.py
+++ oo2/abc.py
@@ -1,7 +1,24 @@
-from abc import ABC
+'''from abc import ABC
 from collections.abc import MutableSequence
 from numbers import Complex
 
 class Numero(Complex):
     def __getitem__(self, item):
         super().__getitem__()
+'''
+
+from collections.abc import Sized
+
+class MinhaListagem(Sized):
+    def __init__(self, descricao):
+        self.descricao = descricao
+
+    #resolvendo exercício, para executar len precisa do método abaixo adicionado.
+    def __len__(self):
+        return len(self.descricao)
+
+    def __str__(self):
+        return self.descricao
+
+lista = MinhaListagem('Nova_lista')
+print(lista)
