c193af571ec20a142a1afcc38ac032424062982c
Marcus Bernardo <84023514+Marcus-Bernard0@users.noreply.github.com>
2022-06-23 18:16:10 -0300

    organizando os códigos

--- extrator_url/main.py
+++ extrator_url/main.py
@@ -1,40 +1,30 @@
-url = 'https//bytebank.com/cambio?moedaOrigem=real'
+url = 'https//bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real'
 print(url)
-
-
-'''#fatiamento
-texto = 'abcde'
-print(texto[0])
-
-print(texto[0:2])'''
-
-#método find para exibir posição
-'''texto = 'abcde'
-encontrando = texto.find('b')
-print(encontrando)'''
-
-
 
 
 indice_interrogacao = url.find('?')
 print(indice_interrogacao)
 
-#deixando o código mais dinâmico
+#Separa a base
 url_base = url[:indice_interrogacao]
 print(url_base)
+
 
 url_parametros = url[indice_interrogacao + 1:]
 print(url_parametros)
 
-#extraindo a moeda
-parametro = ('moedaOrigem')
-parametro_busca =  url.find('moedaOrigem')
+#Encontrando posição do paramêtro
+parametro = ('quantidade')
+parametro_busca =  url.find(parametro)
 print(parametro_busca)
 
-#obtendo o tamanho do parametro
-tamanho_parametro = len(parametro)
-print(tamanho_parametro)
+indice_valor = parametro_busca + len(parametro) + 1
+indice_comercial = url.find('&', indice_valor)
 
-#formula para extrair o a modeda após o =
-indice_valor = parametro_busca + tamanho_parametro + 1
-print(indice_valor)
+#condição para extrair a moeda destino e origem e outros paramêtros
+if indice_comercial == -1:
+    valor = url[indice_valor:]
+    print(valor)
+else:
+    valor = url[indice_valor:indice_comercial]
+    print(valor)
---
+++ extrator_url/main_comentado.py
@@ -0,0 +1,50 @@
+url = 'https//bytebank.com/cambio?moedaDestino=dolar&moedaOrigem=real'
+print(url)
+
+
+'''#fatiamento
+texto = 'abcde'
+print(texto[0])
+
+print(texto[0:2])'''
+
+#método find para exibir posição
+'''texto = 'abcde'
+encontrando = texto.find('b')
+print(encontrando)'''
+
+
+
+
+indice_interrogacao = url.find('?')
+print(indice_interrogacao)
+
+#deixando o código mais dinâmico
+url_base = url[:indice_interrogacao]
+print(url_base)
+
+url_parametros = url[indice_interrogacao + 1:]
+print(url_parametros)
+
+#encontrando posição
+parametro = ('moedaOrigem')
+parametro_busca =  url.find('moedaOrigem')
+print(parametro_busca)
+
+'''obtendo o tamanho do parametro
+tamanho_parametro = len(parametro)
+print(tamanho_parametro)'''
+
+'''#alternativa
+indice_valor = parametro_busca + tamanho_parametro + 1
+print(indice_valor)'''
+
+#formula para extrair o a modeda após o =
+indice_valor = parametro_busca + len(parametro) + 1
+#print(indice_valor)
+
+
+valor = url[indice_valor:]
+print(valor)
+
+#
