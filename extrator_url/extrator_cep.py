e5abb0fefa25cdd5d8b7e51ad09a9b0d9e37aeea
Marcus Bernardo <marcusbernardo58@gmail.com>
2022-06-25 19:32:53 -0300

    melhorando padrões

--- extrator_url/extrator_cep.py
+++ extrator_url/extrator_cep.py
@@ -1,11 +1,11 @@
-endereco = "Rua Bororós 30, apartamento 312, Vila Togni, Poços de caldas, Mg, 37704350"
+endereco = "Rua Bororós 30, apartamento 312, Vila Togni, Poços de caldas, Mg, 37704-350"
 
 import re 
 
 #Cep 5 digitos + [-] + 3 digitos ou Cep 8 dígitos
 
 
-padrao = re.compile("[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]")
+padrao = re.compile("[0-9]{5}[-]{0,1}[0-9]{3}")
 
 #verificando se o padrão foi encontrado
 busca = padrao.search(endereco)
